20e5fdfa4e564a20fe35d54317cc10fa00a72ef0
Amar Sahinovic <amarsahinovic@gmail.com>
2011-09-25 00:05:37 +0200

    Modified urls.py for a better way to iclude root app urls

--- urls.py
+++ urls.py
@@ -14,20 +14,16 @@ from django.conf import settings
 urlpatterns = patterns('',
     (r'^app1/',    include('apps.app1.urls')),
     # Add your apps here
+    
+    # Root app should go last
+    (r'^',    include('apps.core.urls')),
+    
 )
 
 
-# Following two settings are for serving static files in development, 
+# Following settings are for serving static files in development, 
 # and are only active if DEBUG = True
 # Static files, from static/ directory
 urlpatterns += staticfiles_urlpatterns()
 # Media files, from media/ directory
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-# Root of the website, needs to be last and added like this
-# We can't use (r'^/', include('apps.core.urls')) for root because this will not
-# work for homepage, where url is basically '', and we are trying to capture '/' url, 
-import apps.core.urls
-urlpatterns += apps.core.urls.urlpatterns
-
