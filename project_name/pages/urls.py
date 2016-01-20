0eefb172da0ab9730f9a302bb0d48386175111c5
Amar Sahinovic <amarsahinovic@gmail.com>
2016-01-20 20:38:25 +0100

    Remove patterns from urls.py

--- project_name/pages/urls.py
+++ project_name/pages/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns
 from django.conf.urls import url
 
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
 
     url(r'^$',
         views.HomepageView.as_view(),
         name='homepage',),
-)
+]
--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
@@ -6,7 +6,7 @@ from django.conf import settings
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     url(r'^', include('pages.urls')),
     url(r'^foundation/', include('foundation.urls')),
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
